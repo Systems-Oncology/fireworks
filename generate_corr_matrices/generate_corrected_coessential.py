80e7bcdbb4f5e7c51747336a34e8dfdd6ab3579a
so-susan <susan@systemsoncology.com>
2021-02-02 11:39:33 -0700

    Try except to identify missing genes or annotation mismatch
    Some genes in the CERES file appear to be missing from the biomart annotations file. Generated a list of these and retaining code for now in case we want to execute the code omitting these in this "debug" branch.

--- generate_corr_matrices/generate_corrected_coessential.py
+++ generate_corr_matrices/generate_corrected_coessential.py
@@ -2,25 +2,44 @@ import pandas as pd
 import numpy as np
 import os
 
-df_wide = pd.read_csv('20q4_cleaned.csv', index_col=0) #wide format, cell lines index, gene symbols as columns
+path = '~/Systems Oncology, LLC/Mine - Documents/Projects/EXP_002_Differential_Fitness_Oncogenic_Driver_Mut/Design/'
+pathname = os.fspath(path)
+
+filename = 'pivoted_CERES_FC_w_DepMap_ID.csv'
+
+filepathname = pathname + filename
+
+df_wideT = pd.read_csv(filepathname, index_col=0) #wide format, cell lines index, gene symbols as columns
+df_wide = df_wideT.T 
 df1 = pd.read_csv('genome_annotations_biomart_updated.csv')
 dgd = pd.read_csv('dgd_Hsa_all_v71.tsv', sep='\t', index_col='Name')
 
 rank_threshold = 20 #on each side
 dup_genes = dgd.index.tolist()
 
+missing_genes = []
+
 a = pd.DataFrame()
 for gene in df_wide.columns.tolist():
-	if gene in dup_genes:
-		a[gene] = df_wide[gene] #just copy the original essentiality score if a duplicate gene
-	else:
-		chromosome = df1[df1['Gene'] == gene].Chromosome.values[0]
-		df2 = df1[df1['Chromosome'] == chromosome].sort_values(by='End').reset_index()
-		pos = df2[df2['Gene'] == gene].index[0]
-		locus_genes = df2.loc[pos-rank_threshold:pos+rank_threshold, :].Gene.tolist()
-		locus_genes.remove(gene)
+    if gene in dup_genes:
+        a[gene] = df_wide[gene] #just copy the original essentiality score if a duplicate gene
+    else:
+        try: 
+            chromosome = df1[df1['Gene'] == gene].Chromosome.values[0]
+        except: 
+            print(gene)
+            missing_genes.append(gene)
+        else:
+            df2 = df1[df1['Chromosome'] == chromosome].sort_values(by='End').reset_index()
+            pos = df2[df2['Gene'] == gene].index[0]
+            locus_genes = df2.loc[pos-rank_threshold:pos+rank_threshold, :].Gene.tolist()
+            locus_genes.remove(gene)
 
-		a[gene] = df_wide[gene] - (df_wide[[i for i in locus_genes if i in df_wide]].median(axis=1)/2)
+            a[gene] = df_wide[gene] - (df_wide[[i for i in locus_genes if i in df_wide]].median(axis=1)/2)
+
+ with open('missing_genes.txt', 'w') as filehandle:
+    for listitem in missing_genes:
+        filehandle.write('%s\n' % listitem)
 
 b = a.corr()
 b.to_hdf('corrected_coessentiality_matrix.h5', key='stage', mode='w')
